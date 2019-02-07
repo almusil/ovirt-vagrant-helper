7d6b4a946c8767d6cbce9db02b1628ee501f7614
Ales Musil <amusil@redhat.com>
2019-02-07 11:00:06 +0100

    Add basic answer file for engine setup
    - Add script that automatically install engine
    with provided answer.conf file
    
    - Move scripts under common folder that they can be reused
    in future
    
    - Fix wrong engine IPv6 addresses

--- common/scripts/engine_setup.sh
+++ common/scripts/engine_setup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -xe
 
+ANSWER_LOCATION="/tmp/answer.conf"
+
+yum -y install ovirt-engine
+
+engine-setup --config-append=${ANSWER_LOCATION} --accept-defaults
+
 cat << EOF > /etc/ovirt-engine/engine.conf.d/11-disable_sso.conf
 SSO_CALLBACK_PREFIX_CHECK=false
 EOF
